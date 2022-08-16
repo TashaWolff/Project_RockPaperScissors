be11da5653b00ef5c3dcec30c69c38aac40bf43e
Tasha Wolff <tashanwolff@gmail.com>
2022-08-16 15:15:11 -0700

    Add function to determine winner of round

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,32 @@
 function getComputerChoice() {
     let choice = Math.random() * 100;
     if (choice > 0 && choice < 33) {
-        return "Rock"
+        return "rock"
     } else if (choice >= 33 && choice < 66) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerSelection) {
+    let result;
+
+    if (playerChoice === "rock" && computerSelection === "scissors") {
+        result = "winner";
+    } else if (playerChoice === "rock" && computerSelection === "rock") {
+        result = "tie";
+    } else if (playerChoice === "paper" && computerSelection === "rock") {
+        result = "winner";
+    } else if (playerChoice === "paper" && computerSelection === "paper") {
+        result = "tie";
+    } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        result = "winner";
+    } else if (playerChoice === "scissors" && computerSelection === "scissors") {
+        result = "tie";
+    } else {
+        result = "lose";
+    }
+
+    return giveMessage();
+}
