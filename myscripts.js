e7ee885d0c5f2dcfa04715129342e711f54f724a
Tasha Wolff <tashanwolff@gmail.com>
2022-08-16 14:17:03 -0700

    Add function to generate random computer selection

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+    let choice = Math.random() * 100;
+    if (choice > 0 && choice < 33) {
+        return "Rock"
+    } else if (choice >= 33 && choice < 66) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice());
